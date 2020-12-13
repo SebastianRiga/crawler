fad4a644e75cb0e5308b60980753be0bb333637d
Sebastian Riga <sebastian.riga.development@gmail.com>
2020-12-13 01:45:40 +0100

    Updated npm scripts | Added app.ts | Added index.html | Added styles.css | Added .gitkeeps for folder structures | Added .gitattributes for images | Updated dependencies

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import * as Phaser from 'phaser';
+
+/**
+ * Central game configuration object for Crawler.
+ */
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { 
+                y: 0 
+            },
+            debug: true
+        }
+    },
+    scene: [],
+    scale: {
+        zoom: 2
+    }
+};
+
+export const game = new Phaser.Game(gameConfig);
